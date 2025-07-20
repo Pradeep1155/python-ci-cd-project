14e3ee4d12774abf3d10c9cae2821f35acb11913
root <root@localhost.localdomain>
2025-07-20 17:10:20 +0530

    WIP: local changes before pulling

--- test_app.py
+++ test_app.py
@@ -1,5 +1,13 @@
-from app import add
+from app import app
+
+def test_home():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert b"Welcome to Visvasa" in response.data
 
 def test_add():
-    assert add(2, 3) == 5
+    tester = app.test_client()
+    response = tester.get('/add/2/3')
+    assert b'Result: 5' in response.data
 
