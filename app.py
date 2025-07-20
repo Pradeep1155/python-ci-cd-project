83464a1bbb0afa9c2649da6817636d8c7c66abef
root <root@localhost.localdomain>
2025-07-20 17:07:35 +0530

    Fix import Flask

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-def add(a, b):
-    return a + b
+from flask import Flask  
 
-if __name__ == "__main__":
-    print("Addition:", add(5, 3))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to Visvasa CI/CD Flask App!"
+
+@app.route('/add/<int:a>/<int:b>')
+def add_route(a, b):
+    return f"Result: {a + b}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
 
